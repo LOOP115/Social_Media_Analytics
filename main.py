c5ca3b92667cea16f7fec15bff82137f347c6811
LOOP <cjhm000115@gmail.com>
2023-03-19 17:51:26 +1100

    Bug fix

--- main.py
+++ main.py
@@ -3,7 +3,32 @@ import pandas as pd
 import ijson
 from collections import Counter, defaultdict
 import time
+import re
 
+
+gcc_list = [
+    "1gsyd",
+    "2gmel",
+    "3gbri",
+    "4gade",
+    "5gper",
+    "6ghob",
+    "7gdar",
+    "8acte",
+    "9oter"
+]
+
+gcc_full_list = [
+    "1gsyd (Greater Sydney)",
+    "2gmel (Greater Melbourne)",
+    "3gbri (Greater Brisbane)",
+    "4gade (Greater Adelaide)",
+    "5gper (Greater Perth)",
+    "6ghob (Greater Hobart)",
+    "7gdar (Greater Darwin)",
+    "8acte (Greater Canberra)",
+    "9oter (Greater Other Territories)"
+]
 
 state_dict = {
     '1': 'New South Wales',
@@ -17,9 +42,15 @@ state_dict = {
     '9': 'Other Territories'
 }
 
+rural_pattern = r'^.[r].*'
+
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
+
+
+def create_int_dict():
+    return defaultdict(int)
 
 
 def load_twitter_data(file):
@@ -35,46 +66,41 @@ def load_twitter_data(file):
                 'suburb': full_name[0],
                 'state': full_name[1] if len(full_name) > 1 else '',
             })
-        data = pd.DataFrame(data)
-        data = data.dropna(subset=['author_id'])
         return data
 
 
 def load_sal_data(file):
-    data = []
+    data = {}
     with open(file, 'r', encoding='utf-8') as file:
         suburbs_data = ijson.items(file, '')
         for suburbs in suburbs_data:
             for suburb, values in suburbs.items():
-                data.append({
-                    'suburb': suburb,
+                data[suburb.lower()] = {
                     'ste': values['ste'],
                     'gcc': values['gcc'],
                     'sal': values['sal'],
                     'ste_name': state_dict[values['ste']]
-                })
-    return pd.DataFrame(data)
+                }
+    return data
 
 
 def analyze(twitter_data, sal_data):
     res = {
-        'tweets_count': 0,
+        'tweets_cnt': [0] * 9,
         'top_users': Counter(),
-        'cities_users': defaultdict(set)
+        'cities_users': defaultdict(create_int_dict)
     }
-    for _, tweet in twitter_data.iterrows():
+    for tweet in twitter_data:
         author_id = tweet['author_id']
-        suburb = tweet['suburb']
-        state = tweet['state']
-        matched_suburb = sal_data[(sal_data['suburb'].str.lower() == suburb.lower()) &
-                                  (sal_data['ste_name'].str.lower() == state.lower())]
-        if not matched_suburb.empty:
-            gcc = matched_suburb['gcc'].iloc[0]
-            # Ignore rural areas
-            if not gcc.startswith("1r"):
-                res['tweets_count'] += 1
+        suburb = tweet['suburb'].lower()
+        state = tweet['state'].lower()
+
+        if suburb in sal_data.keys() and sal_data[suburb]['ste_name'].lower() == state:
+            gcc = sal_data[suburb]['gcc']
+            if not re.match(rural_pattern, gcc):
+                res['tweets_cnt'][int(gcc[0]) - 1] += 1
                 res['top_users'][author_id] += 1
-                res['cities_users'][author_id].add(gcc)
+                res['cities_users'][author_id][gcc] += 1
     return res
 
 
@@ -89,7 +115,8 @@ def parallel_processing(data):
 
 
 def main():
-    twitter_data = load_twitter_data('data/twitter-data-small.json')
+    start_time = time.time()
+    twitter_data = load_twitter_data('data/tinyTwitter.json')
     sal_data = load_sal_data('data/sal.json')
 
     twitter_data_chunk = parallel_processing(twitter_data)
@@ -99,22 +126,25 @@ def main():
 
     if rank == 0:
         stat = {
-            'tweets_count': 0,
+            'tweets_cnt': [0] * 9,
             'top_users': Counter(),
-            'cities_users': defaultdict(set)
+            'cities_users': defaultdict(create_int_dict)
         }
 
         for result in results:
-            stat['tweets_count'] += result['tweets_count']
+            stat['tweets_cnt'] = [stat['tweets_cnt'][i] + result['tweets_cnt'][i] for i in range(9)]
             stat['top_users'] += result['top_users']
 
             for author_id, cities in result['cities_users'].items():
                 stat['cities_users'][author_id].update(cities)
 
         top_users = stat['top_users'].most_common(10)
-        most_cities_users = sorted(stat['cities_users'].items(), key=lambda x: len(x[1]), reverse=True)[:10]
+        most_cities_users = dict(sorted(stat['cities_users'].items(), key=lambda x: (len(x[1]), stat['top_users'][x[0]]), reverse=True))
 
-        print("Number of tweets in Greater Capital cities:", stat['tweets_count'])
+        print("Task 1: Count the number of different tweets made in the Greater Capital cities of Australia")
+        gcc_cnt = {"Greater Capital City": gcc_full_list, "Number of Tweets Made": stat["tweets_cnt"]}
+        df_gcc_cnt = pd.DataFrame(data=gcc_cnt)
+        print(df_gcc_cnt.to_string(index=False))
 
         print("\nTop 10 tweeters")
         cnt = 1
@@ -124,9 +154,15 @@ def main():
 
         cnt = 1
         print("\nTop 10 tweeters making tweets from the most different locations")
-        for author_id, cities in most_cities_users:
-            print(f"#{cnt} {author_id}: {cities}")
+        for author_id, cities in most_cities_users.items():
+            if cnt == 11:
+                break
+            print(f"#{cnt} {author_id}: {stat['top_users'][author_id]} {cities.items()}")
             cnt += 1
+
+
+    end_time = time.time()
+    print(f"\nExecution time: {round((end_time - start_time), 2)}s")
 
 
 if __name__ == '__main__':
